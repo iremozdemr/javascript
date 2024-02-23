0661dcd13ce6ea741cb06881a3ef8c6e53208c38
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-02-23 17:34:23 +0300

    added tutorials

--- tutorial1/script2.js
+++ tutorial1/script2.js
@@ -51,3 +51,6 @@ console.log(typeof 10n);            //bigint
 console.log(typeof 0);              //number
 console.log(typeof true);           //boolean
 console.log(typeof "hello");        //string
+
+console.log(typeof(2));
+console.log(typeof 2);
